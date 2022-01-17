cb12806c28820e265065e2618ad0a3053a4c7063
Emmanuel Oteng Wilson <hagioswilson@gmail.com>
2022-01-17 07:09:58 +0000

    added response types

--- app/Http/Controllers/AchievementsController.php
+++ app/Http/Controllers/AchievementsController.php
@@ -3,10 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\AddCommentRequest;
+use App\Http\Resources\AchievementResponse;
 use App\Models\Lesson;
 use App\Models\User;
 use App\Services\AchievementService;
-use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
 
 class AchievementsController extends Controller
 {
@@ -17,18 +18,18 @@ class AchievementsController extends Controller
         $this->achievementService = $achievementService;
     }
 
-    public function index(User $user)
+    public function index(User $user): AchievementResponse
     {
        return $this->achievementService->index($user);
     }
 
-    public function addComment(AddCommentRequest $request)
+    public function addComment(AddCommentRequest $request): JsonResponse
     {
         return $this->achievementService->addComment($request);
     }
 
-    public function watchLesson(Lesson $lesson)
+    public function watchLesson(Lesson $lesson): JsonResponse
     {
-        return $this->achievementService->watchLesson($lesson);;
+        return $this->achievementService->watchLesson($lesson);
     }
 }
